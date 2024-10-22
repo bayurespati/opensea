136f1b5eb17d278806b4723f848de3529ab11be7
bayurespati <respatibayu48@gmail.com>
2024-10-22 08:45:10 +0700

    add date user created on table user

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -22,22 +22,24 @@
                     <i class="icon-keyboard_arrow_down"></i>
                     <div class="content">
                         <div class="table-heading">
-                            <div class="column" style="width: 10% !important;">NO</div>
+                            <div class="column" style="width: 5% !important;">NO</div>
                             <div class="column" style="width: 20% !important;">NAMA</div>
-                            <div class="column" style="width: 30% !important;">EMAIL</div>
-                            <div class="column" style="width: 20% !important;">TIPE</div>
-                            <div class="column" style="width: 20% !important;">ROLE</div>
-                            <div class="column" style="width: 10% !important;">AKSI</div>
+                            <div class="column" style="width: 35% !important;">EMAIL</div>
+                            <div class="column" style="width: 15% !important;">TANGGAL</div>
+                            <div class="column" style="width: 10% !important;">TIPE</div>
+                            <div class="column" style="width: 10% !important;">ROLE</div>
+                            <div class="column" style="width: 5% !important;">AKSI</div>
                         </div>
 
                         @foreach($users as $key => $user)
                         <div class="table-item">
-                            <div class="column" style="width: 10% !important;">{{ $key+1 }}</div>
+                            <div class="column" style="width: 5% !important;">{{$key+1}}</div>
                             <div class="column" style="width: 20% !important;">{{$user->name}}</div>
-                            <div class="column" style="width: 30% !important;">{{$user->email}}</div>
-                            <div class="column" style="width: 20% !important;">{{$user->is_pins ? "PINS" : "Non PINS"}}</div>
-                            <div class="column" style="width: 20% !important;">{{$user->is_admin ? "Admin" : "User"}}</div>
-                            <div class="column flex gap30" style="width: 10% !important;">
+                            <div class="column" style="width: 35% !important;">{{$user->email}}</div>
+                            <div class="column" style="width: 15% !important;">{{$user->created_at->translatedFormat('d M Y')}}</div>
+                            <div class="column" style="width: 10% !important;">{{$user->is_pins ? "PINS" : "Non PINS"}}</div>
+                            <div class="column" style="width: 10% !important;">{{$user->is_admin ? "Admin" : "User"}}</div>
+                            <div class="column flex gap30" style="width: 5% !important;">
                                 <a <?php echo ("href=/admin/user/edit/" . $user->id) ?> class="icon">
                                     <img src="/assets/icon/custome/edit_white.svg" alt="whatsapp" style="width: 22px;">
                                 </a>
